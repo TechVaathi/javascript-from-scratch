bb7e0e8f98a75bad9d7b5b1fc7eed5cdc6b231c1
Kalyan <justlikethatkalyan@gmail.com>
2025-01-11 23:38:18 +0530

    ep6 code

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-alert("Hi I am Javascript!")
+//Statements 
+
+alert("Hi I am Javascript!");
+
+// Two statements
+
+// alert("Hello, ")
+// alert("World")
+
+
+//New line doesn't mean semicolon always
+
+// alert(3 +
+//     1
+//     + 2);
+
+
+
+// Issue with Javascript example
+// Fails to assume semicolon where its really needed
+
+// alert("Hello");
+// [1, 2].forEach(alert);
+
+
+// Comments
+
+// This comment occupies a line of its own
+// alert('Hello');
+
+// alert('World'); // This comment follows the statement
+
+
+//Strict mode
+
+// 'use strict'
+
+// let num;
+
+// mum = 11;
+// alert(mum)
